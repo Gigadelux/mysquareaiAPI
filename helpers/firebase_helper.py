71ad2e49ecdec09cd4a9edc062b8555af73b5310
Quaz2710 <mirco.delux@gmail.com>
2024-08-27 21:16:56 +0200

    chore: Update firebase_helper.py to use absolute path for service account key

--- helpers/firebase_helper.py
+++ helpers/firebase_helper.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials,auth
 
 class firebase_helper():#TODO make a function that checks if the uuid of user is not null for login function in the api in case of errors
     def __init__(self):
-        service_account_key = "mysquareai-de163-firebase-adminsdk-uy13o-6afe8d3911.json"
+        service_account_key = "/etc/secrets/mysquareai-de163-firebase-adminsdk-uy13o-6afe8d3911.json"
         cred = credentials.Certificate(service_account_key)
         firebase_admin.initialize_app(cred)
         self.db = firestore.Client()
